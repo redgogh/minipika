0c1a7049966cfac04c87b864fb43f7ee043adf12
404NotFoundx <spacecloud@foxmail.com>
2019-11-15 00:28:56 +0800

    update

---
+++ src/main/java/com/tengu/db/Criteria.java
@@ -0,0 +1,20 @@
+package com.tengu.db;
+
+/**
+ * Criteria能够简化SQL
+ * @author 404NotFoundx
+ * @date 2019/11/15 0:17
+ * @version 1.0.0
+ * @since 1.8
+ */
+public class Criteria {
+
+    public Criteria update() {
+        return this;
+    }
+
+    public Criteria insert(Object target) {
+        return this;
+    }
+
+}
