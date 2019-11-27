0f60e555f35ff5340fd1cb29f1093b5388a2188a
404NotFoundx <spacecloud@foxmail.com>
2019-11-27 22:22:01 +0800

    update

---
+++ src/main/java/com/tractor/db/SaviorJdbc.java
@@ -0,0 +1,25 @@
+package com.tractor.db;
+
+/**
+ * @author 404NotFoundx
+ * @date 2019/11/27 22:20
+ * @version 1.0.0
+ * @since 1.8
+ */
+public class SaviorJdbc implements SaviorJdbcService {
+
+    @Override
+    public void rollback() {
+
+    }
+
+    @Override
+    public void rollback(int step) {
+
+    }
+
+    @Override
+    public void log() {
+
+    }
+}
