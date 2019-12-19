e27a16d823a2f11a4f015e4252714bba2ecec3a8
404NotFoundx <laniakeamly@hotmail.com>
2019-12-19 23:40:14 +0800

    JdbcBuilder封装

--- src/main/java/org/laniakeamly/poseidon/framework/db/JdbcBuilderImpl.java
+++ src/main/java/org/laniakeamly/poseidon/framework/db/JdbcBuilderImpl.java
@@ -3,6 +3,8 @@ package org.laniakeamly.poseidon.framework.db;
 import org.laniakeamly.poseidon.framework.annotation.Valid;
 import org.laniakeamly.poseidon.framework.sql.SqlMapper;
 
+import java.util.List;
+
 /**
  * Create by 2BKeyboard on 2019/12/19 14:20
  */
@@ -15,7 +17,32 @@ public class JdbcBuilderImpl implements JdbcBuilder {
     private JdbcSupport jdbcSupport;
 
     @Override
-    public <T> T queryForObject(SqlMapper mapper) {
-        return (T) jdbcSupport.queryForObject(mapper.getSql(),mapper.getResult(),mapper.getArgs());
+    public <T> T queryForObject(SqlMapper sqlMapper) {
+        return (T) jdbcSupport.queryForObject(sqlMapper.getSql(),sqlMapper.getResult(),sqlMapper.getArgs());
+    }
+
+    @Override
+    public <T> List<T> queryForList(SqlMapper sqlMapper) {
+        return (List<T>) jdbcSupport.queryForList(sqlMapper.getSql(),sqlMapper.getResult(),sqlMapper.getArgs());
+    }
+
+    @Override
+    public String queryForJson(SqlMapper sqlMapper) {
+        return jdbcSupport.queryForJson(sqlMapper.getSql(),sqlMapper.getArgs());
+    }
+
+    @Override
+    public NativePageHelper queryForPage(SqlMapper sqlMapper, NativePageHelper pageVo) {
+        return jdbcSupport.queryForPage(sqlMapper.getSql(),pageVo,sqlMapper.getArgs());
+    }
+
+    @Override
+    public int update(SqlMapper sqlMapper) {
+        return jdbcSupport.update(sqlMapper.getSql(),sqlMapper.getArgs());
+    }
+
+    @Override
+    public int insert(SqlMapper sqlMapper) {
+        return jdbcSupport.insert(sqlMapper.getSql(),sqlMapper.getArgs());
     }
 }
