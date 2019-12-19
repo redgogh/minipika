ee2278e5fc88384ef942f00f99f9a3b220859dda
404NotFoundx <laniakeamly@hotmail.com>
2019-12-20 01:26:15 +0800

    update

--- src/main/test/org/laniakeamly/poseidon/loader/DynamicUpdate.java
+++ src/main/test/org/laniakeamly/poseidon/loader/DynamicUpdate.java
@@ -14,19 +14,27 @@ import java.lang.reflect.Method;
 public class DynamicUpdate {
 
     public static void main(String[] args) throws Exception {
+
+        loader("public static void fuck(){ System.out.println(\"hello world by dynamic create!\"); }");
+        loader("public static void fuck1(){ System.out.println(\"hello world by dynamic create!\"); }");
+
+    }
+
+    public static void loader(String method) throws Exception {
         DynamicUpdate du = new DynamicUpdate();
         String fullClassName = "org.laniakeamly.poseidon.javassist.DynamicUpdate";
         ClassPool pool = ClassPool.getDefault();
         CtClass ctClass = pool.get(fullClassName);
-        CtMethod newMethod = CtNewMethod.make("public static void fuck(){ System.out.println(\"hello world by dynamic create!\"); }", ctClass);
+        ctClass.defrost();
+        CtMethod newMethod = CtNewMethod.make(method, ctClass);
         ctClass.addMethod(newMethod);
-
         PoseidonClassLoader classLoader = PoseidonClassLoader.getClassLoader();
+        // todo exception：Exception in thread "main" java.lang.LinkageError: loader (instance of  org/laniakeamly/poseidon/framework/loader/PoseidonClassLoader): attempted  duplicate class definition for name: "org/laniakeamly/poseidon/javassist/DynamicUpdate"
+        // todo 解决方案：https://blog.csdn.net/is_zhoufeng/article/details/26602689
         Class<?> target = classLoader.findClassByBytes(fullClassName,ctClass.toBytecode());
         Object copy = classLoader.getObject(target,du);
 
         copy.getClass().getDeclaredMethod("fuck").invoke(copy);
-
     }
 
 }
