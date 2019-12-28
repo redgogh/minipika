0c32da02e1eb6839d3749ea360ad1778d27ffebb
404NotFoundx <laniakeamly@hotmail.com>
2019-12-28 19:00:30 +0800

    update

--- src/main/java/org/laniakeamly/dev/package-info.java
+++
@@ -1,4 +0,0 @@
-//
-// 当前正在开发的内容
-//
-package org.laniakeamly.dev;
---
+++ src/main/test/org/laniakeamly/dev/ProductMapper.java
@@ -0,0 +1,14 @@
+package org.laniakeamly.dev;
+
+import org.laniakeamly.poseidon.experiment.ProductModel;
+
+import java.util.List;
+
+/**
+ * Create by 2BKeyboard on 2019/12/28 17:28
+ */
+public interface ProductMapper {
+
+    List<ProductModel> findProductByUUID(String uuid);
+
+}
