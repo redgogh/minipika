128e3bccebcd222d387c064680c27e18c0462374
404NotFoundx <laniakeamly@hotmail.com>
2019-12-30 20:24:37 +0800

    update

--- src/main/java/org/laniakeamly/poseidon/framework/annotation/Valid.java
+++ src/main/java/org/laniakeamly/poseidon/framework/annotation/Valid.java
@@ -3,6 +3,10 @@ package org.laniakeamly.poseidon.framework.annotation;
 import java.lang.annotation.*;
 
 /**
+ * bean容器注入注解
+ *
+ * bean container inject annotation.
+ *
  * Create by 2BKeyboard on 2019/12/9 17:31
  */
 @Target(ElementType.FIELD)
