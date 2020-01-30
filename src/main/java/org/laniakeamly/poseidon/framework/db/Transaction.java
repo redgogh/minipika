493a87cfcd3d00c546f751086dc4331148ba9b6e
404NotFoundx <laniakeamly@hotmail.com>
2020-01-30 13:51:12 +0800

    add transaction

---
+++ src/main/java/org/laniakeamly/poseidon/framework/db/Transaction.java
@@ -0,0 +1,8 @@
+package org.laniakeamly.poseidon.framework.db;
+
+/**
+ * Create by 2BKeyboard on 2020/1/30 13:43
+ */
+public interface Transaction {
+
+}
---
+++ src/main/java/org/laniakeamly/poseidon/framework/db/TransactionManager.java
@@ -0,0 +1,9 @@
+package org.laniakeamly.poseidon.framework.db;
+
+/**
+ * 事务管理器
+ * Create by 2BKeyboard on 2020/1/30 13:41
+ */
+public class TransactionManager implements Transaction{
+
+}
