1269a480c657d52f4658dd4bede4f8b5f43d506a
2B键盘 <ltsloveyellow@qq.com>
2020-06-08 01:23:41 +0800

    新增多线程测试包

---
+++ src/main/test/java/threads/PooledThreadsTest.java
@@ -0,0 +1,30 @@
+package threads;
+
+/*
+ * Copyright (C) 2020 tiansheng All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Creates on 2020/6/8.
+ */
+
+/**
+ * 多线程测试, 测试线程池以及数据源管理器是否存在
+ * 多线程问题
+ *
+ * @author tiansheng
+ */
+public class PooledThreadsTest {
+}
