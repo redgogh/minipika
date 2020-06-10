25cf2b175ac0f1f569b0d8bf02972267ef0e6652
ç2B键盘 <ltsloveyellow@qq.com>
2020-06-10 10:14:43 +0800

    动态sql接口定义

---
+++ src/main/java/org/raniaia/minipika/components/mql/xml/DefaultXmlDynamicMql.java
@@ -0,0 +1,34 @@
+package org.raniaia.minipika.components.mql.xml;
+
+/*
+ * Copyright (C) 2020 tiansheng All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Creates on 2020/6/1.
+ */
+
+import org.raniaia.minipika.components.mql.DynamicMql;
+
+import java.util.List;
+
+/**
+ * @author tiansheng
+ */
+public class DefaultXmlDynamicMql implements DynamicMql {
+  @Override
+  public void setInvoker(List<Class<?>> invoker) {
+  }
+}
