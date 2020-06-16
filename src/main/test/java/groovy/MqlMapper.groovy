38fc1dc3d17bcf078478464bd3cbe862814b64f1
2B键盘 <ltsloveyellow@qq.com>
2020-06-16 13:53:41 +0800

    update

--- src/main/test/java/groovy/MqlMapper.groovy
+++ src/main/test/java/groovy/MqlMapper.groovy
@@ -4,23 +4,26 @@ import org.jiakesimk.minipika.components.annotation.Select
 import org.jiakesimk.minipika.components.mql.groovy.MqlProxy
 import org.junit.Test
 
+import java.lang.reflect.Method
+
 class MqlMapper {
 
   @Select("""
-  select * from user_info where 1=1
-  #if isNotEmpty(user.name)
-    and name = #user.name
-  #end
-  #if user.name != 0 && user.name != null
-    and age > #user.age
-  #end
-  and money >= #user.money
-""")
+    select * from user_info where 1=1
+    #if isNotEmpty(user.name)
+      and name = #user.name
+    #end
+    #if user.name != 0 && user.name != null
+      and age > #user.age
+    #end
+    and money >= #user.money
+  """)
   def findUser(User user) {}
 
   @Test
   void test() {
-    new MqlProxy(this.class)
+    Method method = this.class.getDeclaredMethod("findUser", User.class)
+    println method.getParameters()
   }
 
 }
