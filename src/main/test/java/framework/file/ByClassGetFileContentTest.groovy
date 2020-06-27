f9a81acf2c2d024cb20713fea538979388e5a14c
2B键盘 <ltsloveyellow@qq.com>
2020-06-27 19:42:14 +0800

    test

---
+++ src/main/test/java/framework/file/ByClassGetFileContentTest.groovy
@@ -0,0 +1,41 @@
+package framework.file
+
+import groovy.MqlMapper
+import org.junit.Test
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths;
+
+/*
+ * Copyright (C) 2020 tiansheng All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Creates on 2020/6/27.
+ */
+
+/**
+ * @author tiansheng
+ */
+class ByClassGetFileContentTest {
+
+  @Test
+  void test() {
+    def url = MqlMapper.classLoader.getResource("groovy/MqlMapper.class")
+    println new String(Files.readAllBytes(Paths.get(url.toURI())))
+  }
+
+}
---
+++ src/main/test/java/groovy/AstTest.groovy
@@ -0,0 +1,47 @@
+package groovy
+
+import org.codehaus.groovy.ast.ClassNode
+import org.codehaus.groovy.ast.builder.AstBuilder;
+
+/*
+ * Copyright (C) 2020 tiansheng All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Creates on 2020/6/27.
+ */
+
+/**
+ * @author tiansheng
+ */
+class AstTest {
+
+  @org.junit.Test
+  void test() {
+    AstBuilder builder = new AstBuilder()
+    def nodes = builder.buildFromString("""
+      interface MqlMapper {
+        def findUser(user, wdnmd)
+        def addUser(user)
+      }
+    """)
+    nodes.each {
+      if (it instanceof ClassNode) {
+        println it.name
+      }
+    }
+  }
+
+}
