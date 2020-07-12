bf2fa4f891e5864bd95634a4de753b080ba91a25
2B键盘 <ltsloveyellow@qq.com>
2020-07-12 16:02:51 +0800

    update

---
+++ src/test/groovy/test/Hello.groovy
@@ -0,0 +1,14 @@
+package test
+
+import groovy.User
+import org.junit.Test
+
+class Hello {
+
+  @Test
+  void test() {
+    TestFace face
+    println "Hello world"
+  }
+
+}
---
+++ src/test/groovy/test/TestFace.groovy
@@ -0,0 +1,12 @@
+package test
+
+import org.jiakesimk.minipika.components.annotation.Select
+
+interface TestFace {
+
+  @Select("")
+  def addUser()
+
+  def select()
+
+}
