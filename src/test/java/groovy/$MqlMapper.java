ba23e56a7e13a63b116dda3837510de2efd4a0e0
2B键盘 <ltsloveyellow@qq.com>
2020-07-13 14:27:53 +0800

    删除lib包

--- src/test/java/groovy/$MqlMapper.java
+++ src/test/java/groovy/$MqlMapper.java
@@ -1,4 +1,4 @@
-//package org.minipika.commponents.proxy;
+//package org.minipika.components.proxy;
 //
 //import java.lang.*;
 //import java.util.*;
@@ -6,15 +6,34 @@
 //import java.math.*;
 //
 //import org.jiakesimk.minipika.framework.util.*;
+//import org.jiakesimk.minipika.framework.util.agent.*;
 //
 //@SuppressWarnings("unchecked")
 //public class $MqlMapper {
-//  public Object[] findUser(groovy.User user) {
+//  public Object[] addBatch(java.util.List arg0) {
+//    StringBuilder sql = new StringBuilder();
+//    List arguments = new LinkedList();
+//    sql.append(" ");
+//    sql.append("insert into website_user_info (username,  `password`) values (?,  ?) ");
+//    for (Object user : users) {
+//      Object[] singleArgs = new Object[2];
+//      singleArgs[0] = Fields.getValue(user, "username");
+//      singleArgs[1] = Fields.getValue(user, "password");
+//      arguments.add(singleArgs);
+//    }
+//    sql.append(" ");
+//    Object[] objects = new Object[2];
+//    objects[0] = sql.toString();
+//    objects[1] = arguments;
+//    return objects;
+//  }
+//
+//  public Object[] findUser(java.lang.Object arg0) {
 //    StringBuilder sql = new StringBuilder();
 //    List arguments = new LinkedList();
 //    sql.append(" ");
 //    sql.append("select * from website_user_info where 1=1 ");
-//    if (StringUtils.isNotEmpty(Fields.getValue(user, "username")) && Fields.getValue(user, "username") != null) {
+//    if (AgentStringUtils.isNotEmpty(Fields.getValue(user, "username")) && Fields.getValue(user, "username") != null) {
 //      sql.append("and username = ? ");
 //      arguments.add(Fields.getValue(user, "username"));
 //    }
@@ -25,12 +44,14 @@
 //    return objects;
 //  }
 //
-//  public Object[] addUser(groovy.User user) {
+//  public Object[] updateUser(java.lang.Object arg0, java.lang.Object arg1) {
 //    StringBuilder sql = new StringBuilder();
 //    List arguments = new LinkedList();
 //    sql.append(" ");
-//    sql.append("insert into user (username) values (?) ");
+//    sql.append("UPDATE `website_user_info` SET `username` = ?,  `password` = ? WHERE `id` = ?; ");
 //    arguments.add(Fields.getValue(user, "username"));
+//    arguments.add(Fields.getValue(user, "password"));
+//    arguments.add(id);
 //    sql.append(" ");
 //    Object[] objects = new Object[2];
 //    objects[0] = sql.toString();
@@ -38,17 +59,14 @@
 //    return objects;
 //  }
 //
-//  public Object[] addBatch(java.util.List users) {
+//  public Object[] addUser(java.lang.Object arg0) {
 //    StringBuilder sql = new StringBuilder();
 //    List arguments = new LinkedList();
 //    sql.append(" ");
-//    sql.append("insert into user (username) values (?,  ?) ");
-//    for (Object user : users) {
-//      Object[] singleArgs = new Object[2];
-//      singleArgs[0] = Fields.getValue(user, "username");
-//      singleArgs[1] = Fields.getValue(user, "password");
-//      arguments.add(singleArgs);
-//    }
+//    sql.append("insert into website_user_info (username,  `password`) ");
+//    sql.append("values (?, ?) ");
+//    arguments.add(Fields.getValue(user, "username"));
+//    arguments.add(Fields.getValue(user, "password"));
 //    sql.append(" ");
 //    Object[] objects = new Object[2];
 //    objects[0] = sql.toString();
