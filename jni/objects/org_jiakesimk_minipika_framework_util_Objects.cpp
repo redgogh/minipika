c1c02c1f9c12fbdc884edf8f9b272b8ba702bae3
2B键盘 <ltsloveyellow@qq.com>
2020-07-16 17:44:11 +0800

    '测试JNI'

--- jni/objects/org_jiakesimk_minipika_framework_util_Objects.cpp
+++
@@ -1,8 +0,0 @@
-#include 'org_jiakesimk_minipika_framework_util_Objects.h'
-
-JNIEXPORT jlong JNICALL Java_org_jiakesimk_minipika_framework_util_Objects_sizeof
-  (JNIEnv *,
-  jclass,
-  jobject) {
-    return sizeof(jobject)
-  }
