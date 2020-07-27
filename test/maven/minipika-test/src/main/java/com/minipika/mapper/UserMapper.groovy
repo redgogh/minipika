11058e795bbdbdc5b6d1a17eba9a8ab3ec02af13
2B键盘 <ltsloveyellow@qq.com>
2020-07-28 00:01:41 +0800

    测试groovy——ast获取

--- test/maven/minipika-test/src/main/java/com/minipika/mapper/UserMapper.groovy
+++ test/maven/minipika-test/src/main/java/com/minipika/mapper/UserMapper.groovy
@@ -2,10 +2,12 @@ package com.minipika.mapper
 
 import com.minipika.entity.User
 import org.jiakesimk.minipika.components.annotation.Batch
+import org.jiakesimk.minipika.components.annotation.Mapper
 import org.jiakesimk.minipika.components.annotation.QueryOf
 import org.jiakesimk.minipika.components.annotation.Update
 import org.jiakesimk.minipika.framework.factory.Factorys
 
+@Mapper
 interface UserMapper {
 
   UserMapper mapper = Factorys.forMapper(this)
